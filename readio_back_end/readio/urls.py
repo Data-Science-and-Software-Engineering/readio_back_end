a84a329aebafe45c150c28dc454a0616147a6c9b
luiscvr <luiscarlosvaldezr@gmail.com>
2020-04-26 08:13:30 -0700

    added urls file

---
+++ readio_back_end/readio/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from readio import views
+
+urlpatterns = [
+    path('books/', views.BookList.as_view()),
+    path('books/<int:pk>/', views.BookDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
